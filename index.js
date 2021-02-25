d34480ce2a31a67a8922b7f286a6b8bfe9b2f7c3
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-02-25 15:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 const app = express()
-/*const port = 3000
-*/
+const PORT = process.env.PORT || 5000
 
-app.set('port', (process.env.PORT || 80))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 })
